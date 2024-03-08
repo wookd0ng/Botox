7b4d38a89051bf978f9ecf46669ce79afd60eb64
wookd0ng <wook5307@gmail.com>
2024-03-08 16:55:01 +0900

    [Botox 2차 수정 sql]

--- user.sql
+++ user.sql
@@ -1,42 +1,39 @@
 create database botox;
 use botox;
+# user_totalMannerPoint를 두고 칭찬을 받으면 +되고, 리폿받으면 -가 되서 나중에 user_totalMannerPoint가 5이상이면 무슨 등급, 10이상이면 무슨 등급 이런느낌으로
 
 # 유저
-drop table user;
 CREATE TABLE user (
-    user_id VARCHAR(20) PRIMARY KEY NOT NULL,
+    user_id VARCHAR(20) PRIMARY KEY not null,
     user_pw VARCHAR(20) NOT NULL,
     user_profilepic BLOB,
-    user_carrot_temperature VARCHAR(20),
     user_nickname VARCHAR(20),
     user_connection BOOLEAN,
-    user_report INT,
-    CONSTRAINT chk_carrot_temp CHECK (user_carrot_temperature IN ('cool', 'warm', 'hot'))
+    user_totalMannerPoint INT DEFAULT 0
 );
 
 # 친구
-drop table friend;
-CREATE TABLE friend (
-    user_id VARCHAR(20) REFERENCES user(user_id),
-    friend_id VARCHAR(20) REFERENCES user(user_id),
-    friend_request BOOLEAN,
-    friend_connection BOOLEAN,
-    foreign key (user_id) references user(user_id)
+CREATE TABLE friendShip(
+    requestID VARCHAR(20),
+    acceptID VARCHAR(20),
+    FOREIGN KEY (requestID) REFERENCES user(user_id),
+    FOREIGN KEY (acceptID) REFERENCES user(user_id)
 );
-# 방
-drop table room;
+
+# 친구 테이블 유저테이블에 자체참조하게 하는거 모르겠음
+# 방(방장이름(master)
 CREATE TABLE room (
     room_num INT PRIMARY KEY NOT NULL,
     room_type VARCHAR(20),
     room_capacity_limit INT,
+    room_currentNum INT,
     room_lock_state BOOLEAN,
     room_title VARCHAR(100),
     game_name VARCHAR(20) REFERENCES game(game_name),
     game_user_count INT,
-    user_id VARCHAR(20),
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN key (game_name) references game(game_name),
-    CONSTRAINT chk_room_type CHECK (room_type IN ('chat', 'voice'))
+    room_master VARCHAR(20),
+    FOREIGN KEY (room_master) REFERENCES user(user_id),
+    FOREIGN key (game_name) references game(game_name)
 );
 
 # 게임
@@ -50,7 +47,7 @@ CREATE TABLE game (
 drop table report;
 CREATE TABLE report (
     report_id INT AUTO_INCREMENT PRIMARY KEY,
-    report_date DATE,
+    report_time DATE,
     reporting_user_id VARCHAR(20),
     reported_user_id VARCHAR(20),
     FOREIGN KEY (reporting_user_id) REFERENCES user(user_id),
@@ -58,29 +55,28 @@ CREATE TABLE report (
     feedback_result VARCHAR(100),
     reason_for_report TEXT,
     report_type VARCHAR(50),
-    processing_status VARCHAR(50),
-    additional_info TEXT
-);
+    processing_status VARCHAR(50)
+                    );
+
+
+
 
 
 #  게시글
 drop table post;
 CREATE TABLE post (
     post_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_id VARCHAR(20) REFERENCES user(user_id),
     title VARCHAR(100),
     content TEXT,
     user_id varchar(20),
-    post_date DATE,
-    likes_count INT DEFAULT 0,
+    post_time DATE,
     post_type VARCHAR(20),
     comments_count INT DEFAULT 0,
     foreign key (user_id) references user(user_id)
 
 );
 # 댓글
-drop table botox.comment;
-CREATE TABLE botox.comment (
+CREATE TABLE comment (
     comment_id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT REFERENCES post(post_id),
     author_id VARCHAR(20) REFERENCES user(user_id),
@@ -89,5 +85,3 @@ CREATE TABLE botox.comment (
     foreign key (post_id) references post(post_id)
 
 );
-select * from comment;
-select * from post;
